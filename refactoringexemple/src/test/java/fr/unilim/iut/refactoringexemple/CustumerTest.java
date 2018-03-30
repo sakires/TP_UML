7a5039ecada7a34a8cbadd83ec53771119abab70
sakires <34483208+sakires@users.noreply.github.com>
2018-03-30 15:34:26 +0200

    Add files via upload

--- refactoringexemple/src/test/java/fr/unilim/iut/refactoringexemple/CustumerTest.java
+++ refactoringexemple/src/test/java/fr/unilim/iut/refactoringexemple/CustumerTest.java
@@ -1,5 +1,18 @@
 package fr.unilim.iut.refactoringexemple;
+import static org.junit.Assert.assertEquals;
 
 public class CustumerTest {
-
+	public void statementForOneRegularMovie(){
+		Customer customer = new Customer("Alice");
+        Movie movie = new Movie("The Lord of the Rings", Movie.REGULAR);
+        Rental rental = new Rental(movie, 3); // 3 days rental
+        customer.addRental(rental);
+        
+        String statement = customer.statement();
+        
+        String expected = "Rental Record for Alice\n" +
+                "\tThe Lord of the Rings\t3.5\n" +
+                "Amount owed is 3.5\n";
+        assertEquals(expected, statement);
+	}
 }
