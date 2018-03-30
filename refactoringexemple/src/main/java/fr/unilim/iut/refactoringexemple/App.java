79eac19dced5611a7251cb64ccaf904ec4b20b45
sakires <34483208+sakires@users.noreply.github.com>
2018-03-30 15:14:46 +0200

    Add files via upload

---
+++ refactoringexemple/src/main/java/fr/unilim/iut/refactoringexemple/App.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.refactoringexemple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ refactoringexemple/src/test/java/fr/unilim/iut/refactoringexemple/CustumerTest.java
@@ -0,0 +1,5 @@
+package fr.unilim.iut.refactoringexemple;
+
+public class CustumerTest {
+
+}
